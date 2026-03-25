50b616bbad1edd94c9e8430ce9b94a2427309652
Harsha <harshavardhini@Harshas-MacBook-Air.local>
2026-03-25 19:13:01 +0530

    dashboard

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 // 2️⃣ Middleware to parse JSON (optional but common)
 app.use(express.json());
 
-// 3️⃣ Define a simple route for testing
 app.get('/', (req, res) => {
   res.send('Hello World! Your backend is live 🚀');
 });
@@ -26,10 +25,10 @@ app.get('/profile', (req, res) => {
 });
 
 
-// 4️⃣ Use Render/Environment PORT or fallback to 3000 for local testing
+
 const PORT = process.env.PORT || 3000;
 
-// 5️⃣ Start the server
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
